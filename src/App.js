7f800ad3ef9f52f110c2dee69d16e1cef012d0ae
RufaydaEqailan <rufayda.family@gmail.com>
2023-12-06 12:19:08 +0100

    Dates Reminder Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container } from "react-bootstrap";
+import DatesCount from './component/DatesCount'
+import DatesList from './component/DatesList'
+import DatesAction from './component/DataActions'
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font color-body">
+      <Container className="py-5">
+        <DatesCount />
+        <DatesList />
+        <DatesAction />
+      </Container>
     </div>
   );
 }
---
+++ src/component/DatesCount.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+const DatesCount = () => {
+    return (
+        <Row className="justify-content-center my-2">
+            <Col sm="8" className="font">
+                لديك 4 مواعيد
+            </Col>
+        </Row>
+    )
+}
+
+export default DatesCount
